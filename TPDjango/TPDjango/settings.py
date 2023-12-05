88807f2cb6e790954f8f7cd881859104b1aabe23
Orquito22 <127896223+Orquito22@users.noreply.github.com>
2023-12-05 20:21:27 -0300

    tercer commit

--- TPDjango/TPDjango/settings.py
+++ TPDjango/TPDjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-io!&de9q#n5@8qki8ib@#spy7min!busgx2f!%-6zsv-4sjs)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["orquito.pythonanywhere.com"]
 
